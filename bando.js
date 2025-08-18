4adf22059dc942f32b8d151f802c2a5a54d658ca
Adınız Soyadınız <eposta@adresiniz.com>
2025-08-18 15:04:40 +0300

    Yaptığın değişikliği açıklayan kısa mesaj

--- bando.js
+++ bando.js
@@ -6,3 +6,35 @@ function domClick() {
     // Renk kontrolü için güvenilir bir yöntem kullanın
     this.style.color = (this.style.color === "black") ? "#dc8528" : "black";
 }
+let greeting2 = document.querySelector("#hak");
+greeting2.addEventListener("mouseover", domClick);
+
+function domClick() {
+    console.log("etkinlik denetlendi");
+    // Renk kontrolü için güvenilir bir yöntem kullanın
+    this.style.color = (this.style.color === "black") ? "#dc8528" : "black";
+}
+let greeting3 = document.querySelector("#hiz");
+greeting3.addEventListener("mouseover", domClick);
+
+function domClick() {
+    console.log("etkinlik denetlendi");
+    // Renk kontrolü için güvenilir bir yöntem kullanın
+    this.style.color = (this.style.color === "black") ? "#dc8528" : "black";
+}
+let greeting4 = document.querySelector("#biz");
+greeting4.addEventListener("mouseover", domClick);
+
+function domClick() {
+    console.log("etkinlik denetlendi");
+    // Renk kontrolü için güvenilir bir yöntem kullanın
+    this.style.color = (this.style.color === "black") ? "#dc8528" : "black";
+}
+let greeting5 = document.querySelector("#gal");
+greeting5.addEventListener("mouseover", domClick);
+
+function domClick() {
+    console.log("etkinlik denetlendi");
+    // Renk kontrolü için güvenilir bir yöntem kullanın
+    this.style.color = (this.style.color === "black") ? "#dc8528" : "black";
+}
